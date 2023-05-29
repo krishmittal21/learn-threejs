67a2abb85835106f7dfe751b661cc9268c7e484b
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-29 12:40:57 +0530

    Update main.js

--- . myproject/main.js
+++ . myproject/main.js
@@ -10,11 +10,17 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 renderer.render( scene, camera );
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0xFF6347,wireframe:true } );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347} );
 const torus = new THREE.Mesh( geometry, material );
 scene.add( torus );
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(20,20,20)
+scene.add(pointLight)
 function animate() {
   requestAnimationFrame( animate );
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
   renderer.render( scene, camera );
 }
 animate();
