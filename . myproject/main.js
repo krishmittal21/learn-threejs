5a7cb95f38f307878e674b027adddf528b61b0dd
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-29 13:09:23 +0530

    Update main.js

--- . myproject/main.js
+++ . myproject/main.js
@@ -22,6 +22,14 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200,50)
 scene.add(lightHelper,gridHelper)
 const controls = new OrbitControls(camera,renderer.domElement)
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25,24,24)
+  const material = new THREE.MeshStandardMaterial({color:0xffffff})
+  const star = new THREE.Mesh(geometry,material)
+  const [x,y,z] = Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x,y,z)
+  scene.add(star)
+}
 function animate() {
   requestAnimationFrame( animate );
   torus.rotation.x += 0.01;
