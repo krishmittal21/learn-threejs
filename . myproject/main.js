141cd5a95b27a2e3709c9d16782df74594010d3e
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2023-05-29 13:33:59 +0530

    update main.js

--- . myproject/main.js
+++ . myproject/main.js
@@ -9,6 +9,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
+camera.position.setX(-3);
 renderer.render( scene, camera );
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
 const material = new THREE.MeshStandardMaterial( { color: 0xFF6347} );
@@ -18,10 +19,10 @@ const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(5,5,5)
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight,ambientLight)
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200,50)
-scene.add(lightHelper,gridHelper)
-const controls = new OrbitControls(camera,renderer.domElement)
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200,50)
+// scene.add(lightHelper,gridHelper)
+// const controls = new OrbitControls(camera,renderer.domElement)
 function addStar(){
   const geometry = new THREE.SphereGeometry(0.25,24,24)
   const material = new THREE.MeshStandardMaterial({color:0xffffff})
@@ -31,12 +32,51 @@ function addStar(){
   scene.add(star)
 }
 Array(200).fill().forEach(addStar)
+const spaceTexture = new THREE.TextureLoader().load('space.jpg')
+scene.background = spaceTexture;
+
+const falconTexture = new THREE.TextureLoader().load('falcon.png')
+const falcon = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({map:falconTexture})
+);
+scene.add(falcon)
+const moonTexture = new THREE.TextureLoader().load('moon.jpg')
+const normalTexture = new THREE.TextureLoader().load('normal.jpg')
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({
+    map:moonTexture,
+    normalMap:normalTexture
+  })
+);
+scene.add(moon);
+moon.position.z = 30;
+moon.position.setX(-10);
+falcon.position.z = -5;
+falcon.position.x = 2;
+
+
+function moveCamera(){
+  const t= document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+  falcon.rotation.y += 0.01;
+  falcon.rotation.z += 0.01;
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
 function animate() {
   requestAnimationFrame( animate );
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
-  controls.update()
+  moon.rotation.x += 0.005;
+  //controls.update()
   renderer.render( scene, camera );
 }
 animate();
